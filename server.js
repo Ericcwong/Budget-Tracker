c8cbbc882b2f44b7c688e5bbf752dbd1a046f113
Eric Wong <ericwong12@live.com>
2020-03-31 15:45:02 -0600

    fixed routing naming added manifest.webmanifest file

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose.connect("mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require("./routing/api-routing.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
