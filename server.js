5a041af0c11d2a3799987a6ee2bc0ddfb00b867b
Eric Wong <ericwong12@live.com>
2020-04-02 15:28:36 -0600

    testing server heroku deploy

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://ericcwong-budget-tracker:
 
 // routes
 app.use(require("./routing/api-routing.js"));
-app.use(require("./routing/html-routing"));
+app.use(require("./routing/html-routing.js"));
 app.listen(PORT, () => {
   console.log(`App running on http://localhost:${PORT}`);
 });
