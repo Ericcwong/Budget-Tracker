3196a822603298c7ef0b9b61c114e6fea90f86bd
Eric Wong <ericwong12@live.com>
2020-04-02 14:03:40 -0600

    Code was working needed to add chart.js file to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,8 @@
 const FILES_TO_CACHE = [
     "/",
-    "/index.html",
-    "/index.js",
+    "/js/index.js",
+    "/js/indexedDB.js",
+    "/js/chart.js",
     "/manifest.webmanifest",
     "/styles.css",
     "/icons/icon-192x192.png",
@@ -12,12 +13,12 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 //installing the files
-self.addEventListener("install", (evt) => {
+self.addEventListener("install", evt => {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
             console.log("The files were pre-cached successfully!");
             return cache.addAll(FILES_TO_CACHE);
-        })
+        }).catch(err => console.log(err))
     );
 
     self.skipWaiting();
@@ -25,7 +26,7 @@ self.addEventListener("install", (evt) => {
 
 // Retrieving the files through fetch
 
-self.addEventListener("fetch", (evt) => {
+self.addEventListener("fetch", function (evt){
     if(evt.request.url.includes("/api/")){
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
@@ -46,8 +47,60 @@ self.addEventListener("fetch", (evt) => {
     }
 
     evt.respondWith(
-        caches.match(evt.request).then(response => {
-            return response || fetch(evt.request);
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get("accept").includes("text/html")) {
+              // return the cached home page for all requests for html pages
+              return caches.match("/" );
+            }
+          });
         })
-    );
+      );
 });
+
+// const PRECACHE = "precache-v1";
+// const RUNTIME = "runtime";
+
+// self.addEventListener("install", event => {
+//   event.waitUntil(
+//     caches.open(PRECACHE)
+//       .then(cache => cache.addAll(FILES_TO_CACHE))
+//       .then(self.skipWaiting())
+//   );
+// });
+
+// // The activate handler takes care of cleaning up old caches.
+// self.addEventListener("activate", event => {
+//   const currentCaches = [PRECACHE, RUNTIME];
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+//     }).then(cachesToDelete => {
+//       return Promise.all(cachesToDelete.map(cacheToDelete => {
+//         return caches.delete(cacheToDelete);
+//       }));
+//     }).then(() => self.clients.claim())
+//   );
+// });
+
+// self.addEventListener("fetch", event => {
+//   if (event.request.url.startsWith(self.location.origin)) {
+//     event.respondWith(
+//       caches.match(event.request).then(cachedResponse => {
+//         if (cachedResponse) {
+//           return cachedResponse;
+//         }
+
+//         return caches.open(RUNTIME).then(cache => {
+//           return fetch(event.request).then(response => {
+//             return cache.put(event.request, response.clone()).then(() => {
+//               return response;
+//             });
+//           });
+//         });
+//       })
+//     );
+//   }
+// });
