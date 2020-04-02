6cfa187f39b5a916e6ed0d7437d04956f1ce503a
Eric Wong <ericwong12@live.com>
2020-04-02 14:38:49 -0600

    added heroku server changes

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://ericcwong-budget-tracker:gameboy12@ds335678.mlab.com:35678/heroku_s55ktd28",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    },
+    () => console.log("Connected to DB")
+);
 
 // routes
 app.use(require("./routing/api-routing.js"));
